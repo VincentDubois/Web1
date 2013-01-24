0043e2d59601e623f648f02c47ddc213588cc69f
Vincent Dubois <dubois.marseille@wanadoo.fr>
2013-01-24 12:09:43 +0100

    Version initiale.

---
+++ src/net/web1/Web1Activity.java
@@ -0,0 +1,13 @@
+package net.web1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Web1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
