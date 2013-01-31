be527598484376e677d52a73dfd2f4b3be04032d
simon.lemoine <simon.lemoine@103F11.etu.iut-lens.univ-artois.fr>
2013-01-31 11:58:07 +0100

    Ajout de la vue du fin du mode solo

---
+++ src/net/web1/end.java
@@ -0,0 +1,14 @@
+package net.web1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class end extends Activity{
+
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.end);
+	    }
+	
+
+}
