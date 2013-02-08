77b08f1345b9b0d8bcddeb58c26116c7beb6841a
romain.fauvet <romain.fauvet@103F08.etu.iut-lens.univ-artois.fr>
2013-02-08 10:20:20 +0100

    J'ai créé la structure de données pour la liste des contacts

--- src/net/web1/MenuActivity.java
+++ src/net/web1/MenuActivity.java
@@ -1,12 +1,20 @@
 package net.web1;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MenuActivity extends Activity {
 
+	private ArrayList<Contact> liste;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu);
+		
+		liste = new ArrayList<Contact>();
+		
 }
 }
+
